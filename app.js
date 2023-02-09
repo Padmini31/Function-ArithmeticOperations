db3b4b68bf7fa61b18c41e45133b7038c3fdf56f
Padmini <Simplemini75@gmail.com>
2023-02-09 15:39:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    var sum = a+b;
+    console.log("addition is "+sum);
+}
+add(10,20);
+
+function sub(a,b){
+    var sub = a-b;
+    console.log("substraction is "+sub);
+}
+sub(40,20);
+
+function mul(a,b){
+    var pro = a*b;
+    console.log("multiplication is "+pro);
+}
+mul(10,20);
+
+function div(a,b){
+    var div = a/b;
+    console.log("division is "+div);
+}
+div(40,20);
